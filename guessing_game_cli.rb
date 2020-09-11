a8b0eeb889bb457ae8b5ee42461bbd9050e596e3
Joshua Singleton <jsingle345@gmail.com>
2020-09-11 03:28:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -26,35 +26,3 @@ def get_user_input
   gets.chomp
 end
 
-def bye 
-  puts "Goodbye!"
-end
-require_relative 'spec_helper'
-
-describe "Guessing CLI" do
-
-  context 'user inputs' do
-    it "responds to 'exit'" do
-      expect(self).to receive(:gets).and_return('exit')
-      expect { run_guessing_game }.to output(/Goodbye!/).to_stdout
-    end
-
-    it "responds to an incorrect guess" do
-      allow(self).to receive(:rand).and_return(5)
-
-      expect(self).to receive(:gets).and_return("2")
-
-      expect { run_guessing_game }.to output(/Sorry! The computer guessed 6./).to_stdout
-    end
-
-    it "responds to a correct guess" do
-      allow(self).to receive(:rand).and_return(1)
-
-      expect(self).to receive(:gets).and_return("2")
-
-      expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
-    end
-  end
-
-
-end
